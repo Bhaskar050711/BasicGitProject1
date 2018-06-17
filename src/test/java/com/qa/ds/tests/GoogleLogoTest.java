c1ca5171805dce2de12fc4693ad5fb12cff49dc6
Bhaskar050711 <bhaskarkup@gmail.com>
2018-06-17 07:59:43 +0530

    logotest added

---
+++ src/test/java/com/qa/ds/tests/GoogleLogoTest.java
@@ -0,0 +1,23 @@
+package com.qa.ds.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleLogoTest {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.google.com");
+
+		driver.findElement(By.linkText("Gmail")).click();
+
+		Thread.sleep(3000);
+
+		driver.quit();
+	}
+
+}
